1fb803f1198e9a209010b20bf2d3fac00dd67ef9
chenjd <chen.jiadong@outlook.com>
2019-11-29 17:53:47 +0800

    add baseline

---
+++ Assets/Test/LoadTimelineFromAssetBundle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+public class LoadTimelineFromAssetBundle : MonoBehaviour
+{
+	private AnimationTrack m_animTrack;
+    void OnGUI()
+	{
+		if(GUI.Button(new Rect(100, 100, 100, 100), "Load UI"))
+		{
+			var ab = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/newbundle");
+			var go = ab.LoadAsset("Cube") as GameObject;
+			Instantiate(go, go.transform.position, go.transform.rotation);
+		}
+	}
+
+}
